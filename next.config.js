ca160558e6190f2aaa6a994a92aa22281187fee1
dregul4rdev <d.regul4r.dev@gmail.com>
2025-02-02 16:21:12 -0500

    Update next.config.js
    add base path

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
     output:'export',
-    assetPrefix: isProd ? '/InterdimensialCable/' : undefined,
+     basePath: '/InterdimensialCable',
        eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
